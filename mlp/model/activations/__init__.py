251e39d9762442bdee672a960582332a6f6ec04c
lucas-ht <46166712+lucas-ht@users.noreply.github.com>
2024-07-26 09:45:38 +0200

    Update activations and optimizers to export base class

--- mlp/model/activations/__init__.py
+++ mlp/model/activations/__init__.py
@@ -2,10 +2,12 @@
 This module contains the activation functions used in the neural network.
 """
 
+from .activation import Activation
 from .sigmoid import SigmoidActivation
 from .softmax import SoftmaxActivation
 
 __all__ = [
+    'Activation',
     'SigmoidActivation',
     'SoftmaxActivation',
 ]
--- mlp/model/optimizers/__init__.py
+++ mlp/model/optimizers/__init__.py
@@ -2,8 +2,10 @@
 This module contains the optimizers used in the neural network.
 """
 
+from .optimizer import Optimizer
 from .gradient_descent import GradientDescentOptimizer
 
 __all__ = [
+    'Optimizer',
     'GradientDescentOptimizer',
 ]
